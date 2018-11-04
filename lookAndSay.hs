8e06292bad4b45aadcdc4178fe6a82240caa64da
nadavvitri <nadav325@gmail.com>
2018-11-04 12:36:48 +0200

    [char] replaced to String

--- lookAndSay.hs
+++ lookAndSay.hs
@@ -1,7 +1,7 @@
 -- 1)
 import Data.List
 
-lookAndSay ::(Eq a, Num a, Ord a) => a -> [Char]
+lookAndSay ::(Eq a, Num a, Ord a) => a -> String
 lookAndSay 1 = "1"
 lookAndSay n = if (n <= 0) then "Error" else g (map (\xs -> show (length xs) ++ [head(xs)]) (group (lookAndSay (n-1))))
      where g xs = foldl (\acc x -> acc ++ x) "" xs
