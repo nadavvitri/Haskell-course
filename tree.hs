c1be7f942ad1c7ff63696687145a2769a967c439
nadavvitri <nadav325@gmail.com>
2018-11-06 20:59:22 +0200

    general tree

---
+++ tree.hs
@@ -0,0 +1,12 @@
+data Tree a = Node a [Tree a] | Null deriving (Eq, Show, Ord)
+
+elem' :: (Eq a) => a -> Tree a -> Bool
+elem' y Null = False
+elem' y (Node a xs)
+    | y == a = True
+    | otherwise = foldl (\acc x -> if (elem' y x) == True then True else acc) False xs 
+
+
+sum' :: (Num a) => Tree a -> a
+sum' Null = 0
+sum' (Node x xs) = x + sum (map sum' (xs))
