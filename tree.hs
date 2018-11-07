5542517c83f90af257d39738a0197840060d2314
nadavvitri <nadav325@gmail.com>
2018-11-07 17:56:45 +0200

    another way to elem in tree, using map

--- tree.hs
+++ tree.hs
@@ -6,6 +6,10 @@ elem' y (Node a xs)
     | y == a = True
     | otherwise = foldl (\acc x -> if (elem' y x) == True then True else acc) False xs 
 
+elem'' y Null = False
+elem'' y (Node a xs)
+    | y == a = True     
+    | otherwise = or $ map (elem'' y) xs
 
 sum' :: (Num a) => Tree a -> a
 sum' Null = 0
