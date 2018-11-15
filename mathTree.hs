4ed6abf81f4f3704c925b557b51a7b979154a5ff
nadavvitri <nadav325@gmail.com>
2018-11-15 09:58:44 +0200

    skull Tree, eval

---
+++ mathTree.hs
@@ -0,0 +1,23 @@
+-- 2)
+data Tree a = Plus (Tree a) (Tree a)
+             | Sub (Tree a) (Tree a) 
+             | Div (Tree a) (Tree a) 
+             | Mul (Tree a) (Tree a) 
+             | Pow (Tree a) (Tree a)
+             | Sin (Tree a)
+             | Cos (Tree a)
+             | Const (Maybe a) 
+             | X deriving (Show, Eq)
+
+
+--eval ::  => Tree a -> a -> Maybe a
+eval X x = x
+eval (Const value) x = value
+eval (Plus left right) x = (+) <$> (eval left x) <*> (eval right x)
+eval (Sub left right) x = (-) <$> (eval left x) <*> (eval right x)
+eval (Div left right) x = (/) <$> (eval left x) <*> (eval right x)
+eval (Mul left right) x = (*) <$> (eval left x) <*> (eval right x)
+--eval (Pow left right) x = (^) <$> (eval left x) <*> (eval right x)
+--eval (Sin left) x = (sin) <$> (eval left x)
+--eval (Cos left) x = (cos) <$> (eval left x)
+
