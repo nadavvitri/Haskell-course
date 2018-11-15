dfb8fa08f9d3c481d82224e1787ac6c2ff64b96a
nadavvitri <nadav325@gmail.com>
2018-11-15 16:05:10 +0200

    eval and diff

--- mathTree.hs
+++ mathTree.hs
@@ -21,3 +21,19 @@ eval (Pow left right) x = (**) <$> (eval left x) <*> (eval right x)
 eval (Sin left) x = (sin) <$> (eval left x)
 eval (Cos left) x = (cos) <$> (eval left x)
 
+
+differentiate :: (Fractional a) => Tree a -> Tree a
+differentiate (Const value) = Const (Just 0.0)
+differentiate X =  Const (Just 1.0)
+differentiate (Plus left right) = Plus (differentiate left) (differentiate right)
+differentiate (Sub left right) = Sub (differentiate left) (differentiate right)
+differentiate (Mul left right) = Plus (Mul (differentiate left) right) (Mul left (differentiate right))
+differentiate (Div left right) = differentiate (Mul left (Pow right (Const (Just (-1.0)))))
+differentiate (Pow left right) = Mul right (Pow left (Sub right (Const (Just 1.0))))
+differentiate (Cos left) = Mul (Sub (Const (Just 0.0)) (Sin left)) (differentiate left)
+differentiate (Sin left) = Mul (Cos left) (differentiate left)
+
+
+
+
+
