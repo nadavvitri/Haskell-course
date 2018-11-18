d193eb805884a98794da1a02e6d2c53c2a284283
nadavvitri <nadav325@gmail.com>
2018-11-18 20:57:17 +0200

    last year ex1

--- extra/quiz 1 preparation/ex1.hs
+++ extra/quiz 1 preparation/ex1.hs
@@ -1,3 +1,15 @@
 group :: (Eq a) => [a] -> [[a]]
 group [] = []
 group (x:xs) = (x : takeWhile (== x) xs) : group (dropWhile (== x) xs)
+
+
+data BoolTree a = And (BoolTree a) (BoolTree a) 
+                | Or (BoolTree a) (BoolTree a) 
+                | Not (BoolTree a) 
+                | Leaf a deriving (Show, Eq)
+
+eval :: BoolTree Bool -> Bool
+eval (Leaf a) = a
+eval (And left right) = (eval left) && (eval right)
+eval (Or left right) = (eval left) || (eval right)
+eval (Not left) = not (eval left)
